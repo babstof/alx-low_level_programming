a284ba1e62af5f3088ca69b30db9fca498a90f32
oluwatofunmisin <babstof16@gmail.com>
2023-07-16 12:20:35 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 /**
  * main - the entry point void
  *
- *  Return: always 0
+ * Return: always 0
  */
 
 int main(void)
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
