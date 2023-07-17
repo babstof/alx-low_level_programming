c5d787ac64066cd8a3058099046bcc83db538292
oluwatofunmisin <babstof16@gmail.com>
2023-07-17 04:48:20 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stido.h>
+
+/**
+ * main - the entry point of a function
+ *
+ * Return: equals 0 when successful
+ */
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s)". sizeof(char));
+	printf("size of an int: %lu byte(s)" . sizeof(int));
+	printf("size of a long int: %lu byte(s)" . sizeof(long int));
+	printf("size of a long long: %lu byte(s)" . sizeof(long long int));
+	printf("size of a float: %lu byte(s)" . sizeof(float));
+	return (0);
+}
